299424738be1cc31428d7d34c625cc60351f8536
Jason Galea <jason@lecstor.com>
2018-10-14 22:35:20 +1000

    only include unique chunks in app totals

--- src/add-totals.ts
+++ src/add-totals.ts
@@ -2,25 +2,50 @@ import { Summary } from "./types";
 
 export default function addTotals(summary: Summary) {
   const chunkGroups = { ...summary.chunkGroups };
+
+  for (const groupName in chunkGroups) {
+    if (chunkGroups.hasOwnProperty(groupName)) {
+      let diskSize = 0;
+      let gzipSize = 0;
+
+      const group = chunkGroups[groupName];
+      const { assets } = group;
+      for (const file in assets) {
+        if (assets.hasOwnProperty(file)) {
+          diskSize += assets[file].diskSize;
+          if (assets[file].gzipSize) {
+            gzipSize += assets[file].gzipSize!;
+          }
+          group.diskSize = diskSize;
+          group.gzipSize = gzipSize;
+        }
+      }
+    }
+  }
+
   let totalDiskSize = 0;
   let totalGzipSize = 0;
 
-  for (const groupName in chunkGroups) {
-    let diskSize = 0;
-    let gzipSize = 0;
+  const chunks: {
+    [file: string]: { diskSize: number; gzipSize?: number };
+  } = {};
 
-    const group = chunkGroups[groupName];
-    const { assets } = group;
-    for (const file in assets) {
-      diskSize += assets[file].diskSize;
-      if (assets[file].gzipSize) {
-        gzipSize += assets[file].gzipSize!;
+  for (const groupName in chunkGroups) {
+    if (chunkGroups.hasOwnProperty(groupName)) {
+      const group = chunkGroups[groupName];
+      const { assets } = group;
+      for (const file in assets) {
+        if (assets.hasOwnProperty(file)) {
+          chunks[file] = assets[file];
+        }
       }
-      group.diskSize = diskSize;
-      group.gzipSize = gzipSize;
     }
-    totalDiskSize += group.diskSize || 0;
-    totalGzipSize += group.gzipSize || 0;
+  }
+  for (const chunkName in chunks) {
+    if (chunks.hasOwnProperty(chunkName)) {
+      totalDiskSize += chunks[chunkName].diskSize || 0;
+      totalGzipSize += chunks[chunkName].gzipSize || 0;
+    }
   }
 
   return {
